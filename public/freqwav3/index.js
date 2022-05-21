e5eb73d5f26412e0bdd75cc45c9c9bbf756aa878
hta393939 <hta393939@gmail.com>
2022-05-21 17:36:22 +0900

    download function

--- public/freqwav3/index.js
+++ public/freqwav3/index.js
@@ -1,6 +1,24 @@
-/**
- * @file index.js
+/*!
+ * index.js
+ * (c) 2022- usagi
+ * This is released by the MIT License.
  */
+
+const sp = (v, n = 2) => {
+    return ('' + v).padStart(n, '0');
+};
+
+const filedate = (d = new Date()) => {
+    let s = '';
+    s += `${sp(d.getFullYear(), 4)}`;
+    s += `${sp(d.getMonth() + 1)}`;
+    s += `${sp(d.getDate())}`;
+    s += `_${sp(d.getHours())}`;
+    s += `${sp(d.getMinutes())}`;
+    s += `${sp(d.getSeconds())}`;
+    s += `_${sp(d.getMilliseconds(), 3)}`;
+    return s;
+};
 
 class Misc {
     constructor() {
@@ -25,6 +43,34 @@ class Misc {
             console.log('data fire', arg);
         });
     }
+
+    ready() {
+        {
+            const e = window.iddownload;
+            if (e) {
+                e.addEventListener('click', () => {
+                    const blob = new Blob([]);
+                    this.download(blob, `fw_${filedate()}.dat`);
+                });
+            }
+        }
+    }
+
+/**
+ * ファイルをダウンロードする
+ * @param {Blob} blob 
+ * @param {string} name 
+ */
+    download(blob, name) {
+        {
+            const a = document.createElement('a');
+            a.download = name;
+            a.href = URL.createObjectURL(blob);
+            a.dispatchEvent(new MouseEvent('click'));
+            URL.revokeObjectURL(a.href);
+        }
+    }
+
 }
 
 const misc = new Misc();
